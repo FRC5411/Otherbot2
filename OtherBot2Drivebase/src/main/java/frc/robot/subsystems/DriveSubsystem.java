a87fcdcd03d63b51b2fcc03133e54d935f01bff9
AnshulBompada <ancient.flame1145@gmail.com>
2022-11-12 13:11:21 -0600

    Test code
    test code

--- OtherBot2Drivebase/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ OtherBot2Drivebase/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,21 +1,22 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  private WPI_TalonSRX LeftFront;
-  private WPI_TalonSRX LeftBack;
-  private WPI_TalonSRX RightFront;
-  private WPI_TalonSRX RightBack;
+  private WPI_VictorSPX LeftFront;
+  private WPI_VictorSPX LeftBack;
+  private WPI_VictorSPX RightFront;
+  private WPI_VictorSPX RightBack;
   public DifferentialDrive m_robotdrive;
   
   public DriveSubsystem() {
-    LeftFront = new WPI_TalonSRX(4);
-    LeftBack = new WPI_TalonSRX(2);
-    RightFront = new WPI_TalonSRX(3);
-    RightBack = new WPI_TalonSRX(1);
+    LeftFront = new  WPI_VictorSPX(11);
+    LeftBack = new   WPI_VictorSPX(13);
+    RightFront = new WPI_VictorSPX(10);
+    RightBack = new  WPI_VictorSPX(12);
 
     LeftBack.follow(LeftFront);
     RightBack.follow(RightFront);
