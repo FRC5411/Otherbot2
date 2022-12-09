066a4a20ffe536da7ba3b230edb31c0e3f69957d
AnshulBompada <115746973+AnshulBompada@users.noreply.github.com>
2022-12-08 18:59:50 -0600

    l

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,19 +1,21 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class IntakeSubsystem extends SubsystemBase{
-  private WPI_VictorSPX RightIntake;
-  private WPI_VictorSPX RVertIntake;
-  private WPI_VictorSPX LVertIntake;
+  private WPI_TalonSRX RightIntake;
+  private WPI_TalonSRX RVertIntake;
+  private WPI_TalonSRX LVertIntake;
 
   public IntakeSubsystem() {
-    RightIntake = new WPI_VictorSPX(6);
-    RVertIntake = new WPI_VictorSPX(7);
-    LVertIntake = new WPI_VictorSPX(8);
+    RightIntake = new WPI_TalonSRX(6);
+    RVertIntake = new WPI_TalonSRX(17);
+    LVertIntake = new WPI_TalonSRX(16);
 
     LVertIntake.follow(RVertIntake);
+    LVertIntake.setInverted(true);
     RightIntake.setNeutralMode(NeutralMode.Brake);
     LVertIntake.setNeutralMode(NeutralMode.Brake);
     RVertIntake.setNeutralMode(NeutralMode.Brake);
@@ -32,11 +34,11 @@ public class IntakeSubsystem extends SubsystemBase{
   }
 
   public void uptake(){
-    RVertIntake.set(1);
+    RVertIntake.set(0.5);
   }
 
   public void downtake(){
-    RVertIntake.set(-1);
+    RVertIntake.set(-0.5);
   }
 
   public void novertake(){
